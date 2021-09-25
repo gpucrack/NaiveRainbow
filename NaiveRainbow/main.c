2e6bbcf37524129cacc33d5e092a7fc9de0e3893
truelossless <esteban.gressard@gmail.com>
2021-09-25 19:57:50 +0200

    phase online

--- NaiveRainbow/main.c
+++ NaiveRainbow/main.c
@@ -7,7 +7,7 @@
 #include <assert.h>
 
 // The max password length in the rainbow tables.
-#define MAX_PASSWORD_LENGTH 4
+#define MAX_PASSWORD_LENGTH 2
 
 // The hash function used.
 #define HASH_FUNCTION SHA1
@@ -16,20 +16,25 @@
 #define HASH_LENGTH 20
 
 // The number of rows in the table.
-#define TABLE_M 20
+#define TABLE_M 30
 
 // The size of a chain in the table.
-#define TABLE_T 5
+#define TABLE_T 10000
+
+// The number of tables.
+#define TABLE_COUNT 3
 
 /*
-	The data contained in a rainbow table.
-	It can be either a plain text, or a cipher text.
-*/
-typedef union {
-	char plain_text[MAX_PASSWORD_LENGTH + 1];
-	unsigned char cipher_text[HASH_LENGTH];
+	A rainbow table.
 
-} Data;
+	The endings points are an array of plain texts.
+	The starting point is the plain text of the first raw,
+	as an unsigned int.
+*/
+typedef struct {
+	unsigned int starting_point;
+	char** ending_points;
+} RainbowTable;
 
 /*
 	Returns a char in the [a-zA-Z0-9_-] range given a parameter in the [0-63] range.
@@ -62,12 +67,15 @@ char char_in_range(unsigned char n) {
 
 /*
 	A reduce operation, which returns a plain text
-	for a given cipher text and a given convolution.
+	for a given cipher text and a given `iteration`.
+
+	The nth `iteration` reduction function should give
+	the nth+1 plain text reduction.
 
 	We sum in an array of the size of the plain text and
 	take the modulo to get an alphanumeric char.
 */
-void reduce(unsigned char cipher_text[], unsigned int conv, char* plain_text) {
+void reduce(unsigned char cipher_text[], unsigned int iteration, char* plain_text) {
 	unsigned int sum[MAX_PASSWORD_LENGTH] = { 0 };
 	unsigned int current_index = 0;
 
@@ -76,7 +84,7 @@ void reduce(unsigned char cipher_text[], unsigned int conv, char* plain_text) {
 	}
 
 	for (unsigned int i = 0; i < MAX_PASSWORD_LENGTH; i++) {
-		unsigned int mod = (sum[i] + conv) % 65;
+		unsigned int mod = (sum[i] + iteration) % 65;
 
 		// if mod == 64, skip a character (smaller password)
 		if (mod != 64) {
@@ -88,40 +96,55 @@ void reduce(unsigned char cipher_text[], unsigned int conv, char* plain_text) {
 	plain_text[current_index] = '\0';
 }
 
-// Generates a rainbow table.
-Data** gen_table() {
-	// the chain length should be uneven so it starts and ends with a plain text.
-	assert(TABLE_T % 2 == 1);
 
-	Data** table = malloc(sizeof(Data*) * TABLE_M);
-	assert(table != NULL);
+/*
+	Generates a rainbow table of size `TABLE_M*TABLE_T`, where
+	`TABLE_M` is the number of rows
+	`TABLE_T` is the number of plain texts in a chain.
 
+	The `counter_start` parameter is used to discriminate
+	rainbow tables so they're not all similar.
+*/
+RainbowTable gen_table(unsigned int counter_start) {
+	char** ending_points = malloc(sizeof(char*) * TABLE_M);
+	assert(ending_points != NULL);
+
+	// generate all rows
 	for (unsigned int i = 0; i < TABLE_M; i++) {
-		table[i] = malloc(sizeof(Data) * TABLE_T);
-		assert(table[i] != NULL);
-	}
+		// generate the chain
+		ending_points[i] = malloc(sizeof(char) * MAX_PASSWORD_LENGTH + 1);
+		assert(ending_points[i] != NULL);
 
-	assert(table != NULL);
+		char last_plain_text[MAX_PASSWORD_LENGTH + 1];
+		sprintf(last_plain_text, "%u", counter_start + i);
 
-	for (unsigned int i = 0; i < TABLE_M; i++) {
-		// the starting point increments by one each loop
-		sprintf(table[i][0].plain_text, "%u", i);
+		/*
+			Apply a round of hash + reduce `TABLE_T - 1` times.
+			The chain should look like this:
 
-		for (unsigned int j = 1; j < TABLE_T; j++) {
-			// the uneven links are cipher texts
-			if (j % 2) {
-				// hash the last plain text, giving this cipher text
-				HASH_FUNCTION(table[i][j - 1].plain_text, strlen(table[i][j - 1].plain_text), table[i][j].cipher_text);
-			}
-			// the even links are plain texts
-			else {
-				// reduce the last cipher text, giving this plain text
-				reduce(table[i][j - 1].cipher_text, i, table[i][j].plain_text);
-			}
+			n -> r0(h(n)) -> r1(h(r0(h(n))) -> ...
+		*/
+		for (unsigned int j = 0; j < TABLE_T - 1; j++) {
+			unsigned char cipher_text[HASH_LENGTH];
+			HASH_FUNCTION(last_plain_text, strlen(last_plain_text), cipher_text);
+			reduce(cipher_text, j, last_plain_text);
 		}
+
+		strcpy(ending_points[i], last_plain_text);
 	}
 
-	return table;
+	return (RainbowTable) { counter_start, ending_points };
+}
+
+/*
+	Deletes a table.
+*/
+void del_table(RainbowTable table) {
+	for (unsigned int i = 0; i < TABLE_M; i++) {
+		free(table.ending_points[i]);
+	}
+
+	free(table.ending_points);
 }
 
 // Pretty-prints the hash of a cipher text.
@@ -131,27 +154,142 @@ void print_hash(unsigned char cipher_text[]) {
 	}
 }
 
-// Pretty prints a rainbow table.
-void print_table(Data** table) {
+// Pretty-prints a rainbow table.
+void print_table(RainbowTable table) {
 	for (unsigned int i = 0; i < TABLE_M; i++) {
-		for (unsigned int j = 0; j < TABLE_T - 1; j++) {
-			if (j % 2) {
-				print_hash(table[i][j].cipher_text);
-				printf(" -> ");
-			}
-
-			else {
-				printf("%s -> ", table[i][j].plain_text);
-			}
-		}
-
-		printf("%s\n", table[i][TABLE_T - 1].plain_text);
+		printf(
+			"%d -> ... -> %.*s\n",
+			table.starting_point + i,
+			MAX_PASSWORD_LENGTH,
+			table.ending_points[i]
+		);
 	}
 }
 
+// Pretty prints the rainbow matrix corresponding to a rainbow table.
+void print_matrix(RainbowTable table) {
+	for (unsigned int i = 0; i < TABLE_M; i++) {
+		char plain_text[MAX_PASSWORD_LENGTH + 1];
+		unsigned char cipher_text[HASH_LENGTH];
+		sprintf(plain_text, "%u", table.starting_point + i);
+
+		for (unsigned int j = 0; j < TABLE_T - 1; j++) {
+			HASH_FUNCTION(plain_text, strlen(plain_text), cipher_text);
+			printf("%s -> ", plain_text);
+			print_hash(cipher_text);
+			printf(" -> ");
+			reduce(cipher_text, j, plain_text);
+		}
+
+		printf("%s\n", plain_text);
+
+		// the last plain text/ should be the endpoint
+		assert(!strcmp(plain_text, table.ending_points[i]));
+	}
+}
+
+/*
+	Offline phase of the attack.
+	Generates all rainbow tables needed.
+*/
+void offline(RainbowTable* rainbow_tables) {
+	// make sure the counter of the starting point doesn't overflow
+	// the plain text length
+	char max_counter_length[128];
+	sprintf(max_counter_length, "%u", TABLE_COUNT * TABLE_M);
+	assert(strlen(max_counter_length) <= MAX_PASSWORD_LENGTH);
+
+	for (int i = 0; i < TABLE_COUNT; i++) {
+		rainbow_tables[i] = gen_table(i * TABLE_M);
+	}
+}
+
+/*
+	Online phase of the attack.
+
+	Uses the pre-generated rainbow tables to guess
+	the plain text of the given cipher text.
+
+	Returns in `password` the match if any, or returns an empty string.
+*/
+void online(RainbowTable* rainbow_tables, unsigned char* cipher, char* password) {
+	for (int i = 0; i < TABLE_COUNT; i++) {
+		// iterate column by column, starting from the last plaintext
+		// https://stackoverflow.com/questions/3623263/reverse-iteration-with-an-unsigned-loop-variable
+		for (unsigned int j = TABLE_T - 1; j-- > 0;) {
+			char column_plain_text[MAX_PASSWORD_LENGTH + 1];
+			unsigned char column_cipher_text[HASH_LENGTH];
+			memcpy(column_cipher_text, cipher, HASH_LENGTH);
+
+			// get the reduction of the cipher text corresponding to the current column
+			for (unsigned int k = j; k < TABLE_T - 2; k++) {
+				reduce(column_cipher_text, k, column_plain_text);
+				HASH_FUNCTION(column_plain_text, strlen(column_plain_text), column_cipher_text);
+			}
+			reduce(column_cipher_text, TABLE_T - 2, column_plain_text);
+
+			// iterate through all rows to check if it'a an endpoint
+			for (unsigned int k = 0; k < TABLE_M; k++) {
+				// we found a matching endpoint
+				if (!strcmp(rainbow_tables[i].ending_points[k], column_plain_text)) {
+					// re-construct the chain
+					char chain_plain_text[MAX_PASSWORD_LENGTH + 1];
+					unsigned char chain_cipher_text[HASH_LENGTH];
+					sprintf(chain_plain_text, "%u", rainbow_tables[i].starting_point + k);
+
+					for (unsigned int l = 0; l < j; l++) {
+						HASH_FUNCTION(chain_plain_text, strlen(chain_plain_text), chain_cipher_text);
+						reduce(chain_cipher_text, l, chain_plain_text);
+					}
+					HASH_FUNCTION(chain_plain_text, strlen(chain_plain_text), chain_cipher_text);
+
+					/*
+						The cipher was indeed present in the chain, this was
+						not a false positive from a reduction.
+						We found a plain text that matches the cipher text!
+					*/
+					if (!memcmp(chain_cipher_text, cipher, HASH_LENGTH)) {
+						strcpy(password, chain_plain_text);
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	// no match found
+	password[0] = '\0';
+}
+
 int main() {
-	Data** rainbow_table = gen_table();
-	print_table(rainbow_table);
+	RainbowTable rainbow_tables[TABLE_COUNT];
+	offline(rainbow_tables);
+
+	for (int i = 0; i < TABLE_COUNT; i++) {
+		print_table(rainbow_tables[i]);
+		printf("\n");
+	}
+
+	// to print the full matrix
+	// print_matrix(rainbow_tables[0]);
+
+	char* password = "ll";
+	unsigned char cipher[HASH_LENGTH];
+	HASH_FUNCTION(password, strlen(password), cipher);
+
+	char found[MAX_PASSWORD_LENGTH + 1];
+	online(rainbow_tables, cipher, found);
+
+	if (!strcmp(found, "")) {
+		printf("No password found for the given hash.");
+	}
+	else {
+		printf("Password '%s' found for the given hash!", found);
+	}
+
+	for (int i = 0; i < TABLE_COUNT; i++) {
+		del_table(rainbow_tables[i]);
+	}
 
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,30 +1,112 @@
 # Naive Rainbow
 
-Début d'une implémentation des rainbow tables, en utilisant la fonction de hachage SHA1 avec OpenSSL. Marche sur un CPU.
+Implémentation des rainbow tables, en utilisant la fonction de hachage SHA1 avec OpenSSL. Marche sur un CPU.
 
-Exemple de table générée pour une taille de mot de passe de 4 et une longeur de chaine de 5
+Exemple de crackage de mot de passe :
 ```
-0 -> b6589fc6abdc82cf12099d1c2d4ab994e841c -> Qv2_ -> e54be83d5f56afc2470912a907fccb54a98254e -> Xz9T
-1 -> 356a192b7913b04c54574d18c28d46e6395428ab -> tmP -> e74e5f70b7e4ac1e786c3e614cd5e9828207499 -> uSuz
-2 -> da4b9237bacccdf19c760cab7aec4a8359010b0 -> ILB -> 7887b24bd30e8676c2f27cb9b2179d1932 -> _Pi
-3 -> 77de68daecd823babbb58edb1c8e14d7106e83bb -> 4Tjr -> 9cf6c0b343e154a2e24eb68ec0c36975dc5e9b6d -> Jz6-
-4 -> 1b6453892473a467d07372d45eb05abc2031647a -> BdZp -> bbcaf454b5fc5914f39c556b76a5d769bfe15e -> X3qz
-5 -> ac3478d69a3c81fa62e6f5c3696165a4e5e6ac4 -> f672 -> 9e1e89d6c5a596b6442bc665894b93e7a8b152 -> IevK
-6 -> c1dfd96eea8cc2b62785275bca38ac261256e278 -> gx9R -> 722a3e3ea4bfcf255a295d1799d8e68324c59b -> KgmI
-7 -> 902ba3cda188381594b6e1b452790cc53948fda -> XwcC -> fe16ccfca067302782ee34b9688c1caac98d857f -> B1H0
-8 -> fe5dbbcea5ce7e2988b8c69bcfdfde894aabc1f -> xdJw -> 8d19bfdf3745d2585345ff3b0c9f37dffaa836a -> kqzv
-9 -> ade7c2cf97f75d09975f4d72d1fa6c19f4897 -> Vw5 -> 417342fe8961c8af7d4fdf996fe8edfb22d5fb -> xvPr
-10 -> b1d5781111d84f7b3fe45a852e59758cd7a87e5 -> Yh0K -> d11b76ae754f734d09efd4591fe7f5d46d8e89f -> iTAs
+0 -> ... -> ZJ
+1 -> ... -> e3
+2 -> ... -> VS
+3 -> ... -> eS
+4 -> ... -> qy
+5 -> ... -> 4j
+6 -> ... -> CN
+7 -> ... -> 1T
+8 -> ... -> cI
+9 -> ... -> e3
+10 -> ... -> 0B
+11 -> ... -> 4R
+12 -> ... -> VS
+13 -> ... -> e3
+14 -> ... -> I2
+15 -> ... -> fY
+16 -> ... -> 4R
+17 -> ... -> 0B
+18 -> ... -> 4R
+19 -> ... -> cI
+20 -> ... -> qy
+21 -> ... -> X0
+22 -> ... -> eS
+23 -> ... -> VS
+24 -> ... -> e3
+25 -> ... -> cI
+26 -> ... -> fY
+27 -> ... -> eS
+28 -> ... -> ZJ
+29 -> ... -> fY
+
+30 -> ... -> 4R
+31 -> ... -> e3
+32 -> ... -> eS
+33 -> ... -> 4j
+34 -> ... -> eS
+35 -> ... -> 4R
+36 -> ... -> ZJ
+37 -> ... -> 1T
+38 -> ... -> fY
+39 -> ... -> 4R
+40 -> ... -> Gh
+41 -> ... -> cI
+42 -> ... -> eS
+43 -> ... -> eS
+44 -> ... -> qy
+45 -> ... -> _W
+46 -> ... -> ta
+47 -> ... -> 0B
+48 -> ... -> _W
+49 -> ... -> Gh
+50 -> ... -> 4R
+51 -> ... -> 1T
+52 -> ... -> fY
+53 -> ... -> ta
+54 -> ... -> 0B
+55 -> ... -> 4j
+56 -> ... -> Gh
+57 -> ... -> qy
+58 -> ... -> 4R
+59 -> ... -> 4j
+
+60 -> ... -> CN
+61 -> ... -> 4j
+62 -> ... -> eS
+63 -> ... -> 4R
+64 -> ... -> VS
+65 -> ... -> 1T
+66 -> ... -> 0B
+67 -> ... -> ta
+68 -> ... -> 4R
+69 -> ... -> VS
+70 -> ... -> ZJ
+71 -> ... -> qy
+72 -> ... -> 4R
+73 -> ... -> I2
+74 -> ... -> 4j
+75 -> ... -> Gh
+76 -> ... -> X0
+77 -> ... -> ta
+78 -> ... -> ta
+79 -> ... -> I2
+80 -> ... -> ta
+81 -> ... -> 0B
+82 -> ... -> e3
+83 -> ... -> Gh
+84 -> ... -> cI
+85 -> ... -> cI
+86 -> ... -> X0
+87 -> ... -> CN
+88 -> ... -> 4R
+89 -> ... -> 4R
+
+Password 'll' found for the given hash!
 ```
 
 - Fonctionnalités
 
-    - Génère une table de M lignes et de taille de chaîne T
+    - Phase offline et online: cracke un hash avoir avoir précalculé des tables.
 
 - A faire
-
-    - Attaque online
-    - Ne garder que les *starting points* et les *ending points*
+    - Elimination des chaines redondantes. Pour un mot de passe de 2 charactères et une longueur de chaîne
+    de 10 000, on voit que beaucoup de chaînes fusionnent (donc ont le même point de sortie)
     - stockage sur disque
     - Compression
 
